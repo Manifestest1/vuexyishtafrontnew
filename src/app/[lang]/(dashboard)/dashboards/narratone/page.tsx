6b94e676a06c12da8b2e7eb707219f54869df2ab
Manifestest1 <manifestest1@gmail.com>
2024-07-24 17:18:59 +0530

    'changes'

--- src/app/[lang]/(dashboard)/dashboards/narratone/page.tsx
+++ src/app/[lang]/(dashboard)/dashboards/narratone/page.tsx
@@ -1,5 +1,6 @@
  // MUI Imports
  import Grid from '@mui/material/Grid'
+ import ProtectedRoute from '@/context/ProtectedRoute'
 
  // Components Imports
  import WebsiteAnalyticsSlider from '@views/dashboards/analytics/WebsiteAnalyticsSlider'
@@ -12,11 +13,13 @@
 
 
    return (
+    <ProtectedRoute>
      <Grid container spacing={6}>
        <Grid item xs={12} lg={6}>
          <WebsiteAnalyticsSlider />
        </Grid>
      </Grid>
+     </ProtectedRoute>
    )
  }
 
